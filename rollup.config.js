4664ce47237c7ea66e7a209313cccf7c48f9f825
Bruno Dolenc <bruno.dolenc@kroton.com.br>
2023-04-06 14:32:02 -0300

    Ajuste

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,43 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
-import external from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
+import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from './package.json' assert { type: 'json' };
 
 export default [
-    {
-        input: 'src/index.ts',
-        external: ['styled-components'],
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-                name: 'react-ts-lib'
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            external(),
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: './tsconfig.json' }),
-            postcss(),
-            terser()
-        ],
-    },
-    {
-        input: 'dist/esm/types/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: "esm" }],
-        external: [/\.css$/],
-        plugins: [dts()],
-    },
-]
+  {
+    input: 'src/index.ts',
+    external: ['styled-components'],
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        name: 'react-ts-lib',
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    external: [/\.css$/],
+    plugins: [dts()],
+  },
+];
--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const ButtonCp = styled.button`
     background-color: #FF0000;
-    padding: 15px;
-    border: 0;
-    border-radius: 10px;
+    padding: 15px 30px;
+    border: 1px solid #000000;
+    border-radius: 30px;
+    color: #FFFFFF;
 `;
