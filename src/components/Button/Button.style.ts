995069c9974fd26a31efdbb7154be69d21a603e5
Bruno Dolenc <bruno.dolenc@kroton.com.br>
2023-04-06 13:08:46 -0300

    v1

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const ButtonCp = styled.button`
     background-color: #00FF00;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,12 +3,10 @@ import React, { FC } from 'react';
 import { ButtonCp } from './Button.style';
 import { ButtonProps } from './Button.types';
 
-const Button: FC<ButtonProps>  = ({ type, text, onClick }) => {
-    return (
-        <ButtonCp type="button" className={`Button Button-${type}`} onClick={onClick}>
-            {text}
-        </ButtonCp>
-    )
-};
+const Button: FC<ButtonProps> = ({ type, text, onClick }) => (
+  <ButtonCp type="button" className={`Button Button-${type}`} onClick={onClick}>
+    {text}
+  </ButtonCp>
+);
 
 export default Button;
