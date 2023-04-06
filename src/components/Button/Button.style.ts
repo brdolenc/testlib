833a003b5ca965e7bbbaad35fcfeaec31619c667
Bruno Dolenc <bruno.dolenc@kroton.com.br>
2023-04-06 13:12:48 -0300

    v1

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const ButtonCp = styled.button`
-    background-color: #00FF00;
+    background-color: #FF0000;
     padding: 15px;
+    border: 0;
+    border-radius: 10px;
 `;
