37a134ba4f6dd2710c4b749a04b92dbc430d972f
Bruno Dolenc <bruno.dolenc@kroton.com.br>
2023-04-10 17:18:30 -0300

    Teste

--- dist/cjs/types/src/components/Button/Button.types.d.ts
+++
@@ -1,9 +0,0 @@
-import { MouseEventHandler, ReactNode } from 'react';
-export interface ButtonProps {
-    variant?: 'contained' | 'outlined' | 'ghost';
-    fill?: 'primary' | 'secondary';
-    size?: 'sm' | 'md' | 'lg';
-    icon?: ReactNode | boolean;
-    text: string;
-    onClick: MouseEventHandler<HTMLButtonElement>;
-}
--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,33 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 
-import packageJson from './package.json' assert { type: 'json' };
-
-export default [
-  {
-    input: 'src/index.ts',
-    external: ['styled-components'],
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-        name: 'react-ts-lib',
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      external(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      postcss(),
-      terser(),
-    ],
-  },
-  {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    external: [/\.css$/],
-    plugins: [dts()],
-  },
-];
+export default {
+  input: 'src/index.ts',
+  external: ['react', 'react-dom', 'prop-types', 'styled-components'],
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      sourcemap: true,
+      exports: 'named',
+    },
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    postcss(),
+    terser(),
+  ],
+};
